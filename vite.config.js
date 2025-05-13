1b1e4eed7597327d6b992dc37227833e1b0db894
IMU Framework <imuchien@gmail.com>
2025-05-13 20:49:53 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,9 @@
-export default { root: '.', };
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  root: '.', // 確保 index.html 是在這個目錄下
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  }
+});
